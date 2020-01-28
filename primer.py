7bf816fb48aea649174b0c29930d811f1e7b2de0
Sebastián Sastre <sgu@paucasesnovescifp.cat>
2020-01-28 12:37:03 +0100

    Primer commit local

---
+++ primer.py
@@ -0,0 +1,23 @@
+
+def comprova_si_es_primer (num) : 
+
+	 for x in range (2,num-1) :  
+ 
+	 	if num%x == 0 : return False 
+
+	 return True 
+
+print("Escriu un nombre: ")
+
+n = int (input())
+
+if comprova_si_es_primer(n) == True : 
+
+	print("El nombre és primer")
+
+else : 
+
+	print("El nombre no és primer")
+
+
+input("Atura")
